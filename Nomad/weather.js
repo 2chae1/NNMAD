d0a8983d4a65e5105507ef70dd0b373d47b3be60
leechaewon21 <you@example.com>
2022-04-20 01:14:47 +0900

    Make(Add) weather function

---
+++ Nomad/weather.js
@@ -0,0 +1,21 @@
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=870b315b24acadd29e204234684699ba&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data);
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:first-child");
+
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} - ${data.main.temp}`;
+    })
+}
+
+function onGeoError() {
+    alert("SORRY, I CAN'T FIND YOU");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess,onGeoError);
